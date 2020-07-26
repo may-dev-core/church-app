4f16229be6ca8bc67ca50475c195362021df8521
dev.core.may@gmail.com <dev.core.may@gmail.com>
2020-07-26 15:34:31 +0000

    Removed sqlite database settings

--- config/settings.py
+++ config/settings.py
@@ -77,12 +77,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'church_db',
+        'USER':'churchuser',
+        'PASSWORD':'Church@2020#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 db_from_env = dj_database_url.config()
